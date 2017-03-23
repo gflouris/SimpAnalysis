4f069d03188c6b776ce991b102ffd6eeb2712dab
gflouris <flourisj@gmail.com>
2017-03-23 12:04:39 +0100

    read json in main

--- TreeProducer_AOD/macros/json_parser.cpp
+++ TreeProducer_AOD/macros/json_parser.cpp
@@ -3,49 +3,57 @@
 #include "boost/property_tree/ptree.hpp"
 #include "boost/property_tree/json_parser.hpp"
 #include "boost/optional/optional.hpp"
-using namespace std;
 
-bool Check_Run_Lumi(const int , const int );
+#include <ctime>
+
+using namespace std;
+using boost::property_tree::ptree;
+namespace pt = boost::property_tree;
+
+bool Check_Run_Lumi(const int , const int , pt::ptree);
 
 int json_parser() {
-  cout<<Check_Run_Lumi(273730, 1)<<endl;
-  cout<<Check_Run_Lumi(273730, 2)<<endl;
-  cout<<Check_Run_Lumi(273730, 1814)<<endl;
-  cout<<Check_Run_Lumi(273730, 1815)<<endl;
-  cout<<Check_Run_Lumi(2, 1815)<<endl;
+
+  pt::ptree root;
+  pt::read_json("Cert_271036-284044_13TeV_PromptReco_Collisions16_JSON.txt", root);
+
+
+  cout<<Check_Run_Lumi(273730, 1, root)<<endl;
+  cout<<Check_Run_Lumi(273730, 2, root)<<endl;
+  cout<<Check_Run_Lumi(273730, 1814, root)<<endl;
+  cout<<Check_Run_Lumi(273730, 1815, root)<<endl;
+  cout<<Check_Run_Lumi(2, 1815, root)<<endl;
 
 
   //273725": [[83, 252], [254, 2545]],
-  cout<<Check_Run_Lumi(273725, 255)<<endl;
-  cout<<Check_Run_Lumi(273725, 254)<<endl;
-  cout<<Check_Run_Lumi(273725, 253)<<endl;
-  cout<<Check_Run_Lumi(0, 0)<<endl;
+  cout<<Check_Run_Lumi(273725, 255, root)<<endl;
+  cout<<Check_Run_Lumi(273725, 254, root)<<endl;
+  cout<<Check_Run_Lumi(273725, 253, root)<<endl;
+
+
+  // clock_t begin = clock();
+  // for(int i=0; i<50000; i++){
+  //   Check_Run_Lumi(i, i, root);
+  // }
+  // clock_t end = clock();
+  // double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
+  // cout<<"time  "<<elapsed_secs<<endl;
+
 
   return 0;
 }
 
 
-bool Check_Run_Lumi(const int run, const int lumi_n){
-
-  using boost::property_tree::ptree;
-
-  // Short alias for boost namespace
-  namespace pt = boost::property_tree;
-
-  // Create a root
-  pt::ptree root;
-
-  // Load the json file in this ptree
-  pt::read_json("Cert_271036-284044_13TeV_PromptReco_Collisions16_JSON.txt", root);
+bool Check_Run_Lumi(const int run, const int lumi_n, pt::ptree root){
 
   std::string run_n = std::to_string(run);
 
-  vector<pair<int, int> > lumis;
 
   ///Check if run exists - if not return false
   boost::optional< ptree& > child = root.get_child_optional( run_n );
   if(!child) return false;
 
+  vector<pair<int, int> > lumis;
   ///Store pair of lumis in a vector
   for (pt::ptree::value_type &row : root.get_child(run_n))
   {
